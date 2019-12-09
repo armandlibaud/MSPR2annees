4b0ea3cadcf66f4c2367fa1de111677f65cec505
soulabail <iris.soulabail@ecoles-wis.net>
2019-12-09 16:29:05 +0100

    home

--- home.php
+++ home.php
@@ -1,3 +1,66 @@
 <?php require_once 'includes/header.php'; ?>
 
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="card text-center">
+                <div class="card-header">
+                    <p>Mon profil </p>
+                </div>
+                <div class="card-body">
+                    <h5 class="mb-4">Prénom - Nom de la personne</h5>
+                    <p class="text-left">Fil d'actualité</p>
+                    <p class="text-left">Messages</p>
+                    <p class="text-left">Lorem ipsum </p>
+                    <p class="text-left mb-4"> Lorem ipsum</p>
+                    <p class="text-left font-weight-bold"> Raccourci </p>
+                    <p class="text-left">Lorem ipsum</p>
+                    <p class="text-left">Lorem ipsum</p>
+                    <p class="text-left">Lorem ipsum</p>
+                </div>
+            </div>
+        </div>
 
+
+
+        <div class="col-lg-8">
+            <h2 class="text-center"> Fil d'actualité </h2>
+
+            <div class="card mb-3">
+                <div class="card-body">
+
+                    <form method="post">
+                        <label for="summary text-center"> Exprime toi ! </label>
+                        <textarea name="summary" id="summary"></textarea>
+                    <button> Publier ! </button>
+
+                </div>
+
+            <div class="card mb-3 text-center">
+                <div class="card-body">
+                    <img src="https://placehold.it/300x300">
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                    <img src="https://placehold.it/300x300">
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-body">
+                    <img src="https://placehold.it/300x300">
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-body">
+                    <img src="https://placehold.it/300x300">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<?php require_once 'includes/footer.php'; ?>
