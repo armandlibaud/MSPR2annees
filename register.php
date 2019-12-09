8ba30b3e0edb790c7b777d4a4d3bc16111b9acdf
armand <armandlibaud@gmail.com>
2019-12-09 14:23:59 +0100

    Auto stash before merge of "master" and "origin/master"

--- register.php
+++ register.php
@@ -2,10 +2,13 @@
 session_start();
 // $connexion = new PDO('mysql:host=localhost;dbname=projetphp','root','root');
 
-
-
-
-
+$errors=[];
+$first_name="";
+$last_name="";
+$email="";
+$phone_number="";
+$ville="";
+$tel="";
 ?>
 
 
