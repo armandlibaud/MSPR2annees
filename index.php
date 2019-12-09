62131412f4c6035878d68994d15eae3dd7ca4a3f
armand <armandlibaud@gmail.com>
2019-12-09 11:56:20 +0100

    index

--- index.php
+++ index.php
@@ -14,9 +14,31 @@
 
 <body>
 <main>
-    <h1> Fil d'actualité </h1>
+    <div class="container">
+    <div class="row">
+    <div class= "col-lg-6">
+   <div class="card text-center">
+       <div class="card-header">
+           <p>Se connecter </p>
+       </div>
+       <div class="card-body">
+           <?php require_once 'register.php'; ?>
+       </div>
+   </div>
+    </div>
 
+    <div class= "col-lg-6">
+        <div class="card text-center">
+            <div class="card-header">
+                <p>S'inscrire </p>
+            </div>
+            <div class="card-body">
 
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
 <?php require_once 'includes/footer.php'; ?>
 
 
--- register.php
+++ register.php
@@ -3,16 +3,18 @@
 session_start();
 //$connexion = new PDO('mysql:host=localhost;dbname=mspr','root','root');
 
-$login_admin = "abcd";
-$login_pwd = "abcd";
+$login_admin = "";
+$login_pwd = "";
 
 
 ?>
 
 
     <form action="" method="POST" class="container-fluid text-center mt-3">
-        <input type="text" name="mail">
-        <input type="password" name="mdp">
+        <p class="mt-1 mb-0">Identifiant</p>
+        <input type="text" name="mail" class="form-control">
+        <p class="mt-3 mb-0">Mot de passe</p>
+        <input type="password" name="mdp" class="form-control">
         <input  class="container-fluid mt-3" type="submit" name="valider">
     </form>
 
