b0d1cd53abaa99fc32a4a47fc792113d93732e57
armand <armandlibaud@gmail.com>
2019-12-10 11:24:42 +0100

    Auto stash before merge of "master" and "origin/master"

--- includes/helpers.php
+++ includes/helpers.php
@@ -3,7 +3,7 @@ require_once 'env.php';
 
 function connectDB(){
     try {
-         return new PDO('mysql:host='.localhost.':dbname='.mspr, root, root, array(
+         return new PDO('mysql:host='.localhost.';dbname='.mspr, root, root, array(
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
