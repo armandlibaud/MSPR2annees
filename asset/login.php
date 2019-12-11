3c05b4a7bdb4a9f04324ed73c4bee089cc17fe1e
armand <armandlibaud@gmail.com>
2019-12-11 10:00:13 +0100

    mercredi matin

--- asset/login.php
+++ asset/login.php
@@ -12,13 +12,14 @@ $stmt->bindValue(':email', $data['email']);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+$id = ($user['id']);
+if ($user['password'] === sha1($data['password'])) {
+    $_SESSION['auth_id'] = $user['id'];
+    header("Location: ../home.php?id=$id");
+}else{
+    header('Location: ../index.php');
+}
 
 
-if ($user['password'] === sha1($data['password'])):
-$_SESSION['auth_id'] = $user['id'];
-header('Location: ../home.php');
-exit;
-endif;
 
-echo 'bug';
 
